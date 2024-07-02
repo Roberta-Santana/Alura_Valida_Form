d94abfb98983f827eef03db80660ebe16f9bbdbd
Roberta Santana <g.roberta.s@hotmail.com>
2024-07-01 22:25:04 -0300

    Validação de idade terminada, iniciando mensagens customizadas

--- js/script.js
+++ js/script.js
@@ -1,18 +1,64 @@
 import ehUmCPF from "../js/valida-cpf.js";
+import ehMaoirDeIdade from "./valida-idade.js";
+
 const camposDoFormulario = document.querySelectorAll('[required]');
+const tiposDeErro = ['valueMissing', 'typeMismatch', 'patternMismatch', 'tooShort', 'customError'];
+/* criando uma var que recebe a lista dos erros mais comuns= 
+valueMissing: campo vazio, faltando valor; typeMismatch: erro no tipo de input do campo, ex: email sem @,
+customError: erros curtomizados, relacionados as lógicas de validação das funções ehUmCPF e ehMaiorDeIdade*/
+
+const mensagens = {
+    nome: {
+        valueMissing: "O campo de nome não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um nome válido.",
+        tooShort: "Por favor, preencha um nome válido."
+    },
+    email: {
+        valueMissing: "O campo de e-mail não pode estar vazio.",
+        typeMismatch: "Por favor, preencha um email válido.",
+        tooShort: "Por favor, preencha um e-mail válido."
+    },
+    rg: {
+        valueMissing: "O campo de RG não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um RG válido.",
+        tooShort: "O campo de RG não tem caractéres suficientes."
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+        patternMismatch: "Por favor, preencha um CPF válido.",
+        customError: "O CPF digitado não existe.",
+        tooShort: "O campo de CPF não tem caractéres suficientes."
+    },
+    aniversario: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
+    termos: {
+        valueMissing: 'Você deve aceitar nossos termos antes de continuar.',
+    }
+}
 
 camposDoFormulario.forEach((campo)=>{ 
-    campo.addEventListener('blur', ()=> verificarCampo(campo))
+    campo.addEventListener('blur', ()=> verificarCampo(campo));
     /*cada indice de camposDoFormulario será recuperado pela função forEach(), e chamado de campo
     cada campo terá um eventListener aguardando um blur
     assim que o blur ocorrer, chama a função verificarCampo(), passando o proprio campo para ser verificado*/
+
+    campo.addEventListener("invalid", evento=>evento.preventDefault());
+    /* quando o campo for inváido, não exibir mensagem padrão do js */
 })
 /*quando chama a função verificarCampo(), não está sendo enviado nada. Para uma função de verificação, precisamos do valor daquele campo.
 Então, verificarCampo, vai receber como parâmetro (campo), 
 Assim, quando acontecer algo ali, vai ser chamado ele(campo) pra acontecer aquela outra função, com referência aquele campo específico*/
 
 function verificarCampo(campo){
+    //cada if é uma condição que especifica o campo, garantindo que seja um dos required desejado
     if(campo.name== "cpf" && campo.value.length>=11){
         ehUmCPF(campo);
     }
+    if(campo.name== "aniversario" && campo.value !=" "){
+        ehMaoirDeIdade(campo);
+    }
+     console.log(campo.validity);/*exibe lista c critérios de validação = 
+     ValidityState{ valid:true, quando os critérios são atendidos / valid:false quando não atendidos}*/
 }
---
+++ js/valida-idade.js
@@ -0,0 +1,12 @@
+export default function ehMaoirDeIdade(campo){
+    const dataNascimento = new Date(campo.value);
+    console.log(validaIdade(dataNascimento))
+}
+
+function validaIdade(data){
+    const dataAtual = new Date();//vazia, data de hoje
+    const dataMais18 = new Date(data.getUTCFullYear()+18, data.getUTCMonth(), data.getUTCDate());
+    //pegou os atributos da data/campo passado via parametro, add 18 ao ano, 
+
+    return dataAtual>=dataMais18;//comparou se dataHoje >= 18 anos somados a data de nascimento passada via campo
+}
